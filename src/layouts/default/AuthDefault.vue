a30178ade1f64f6d7fd7a881662a24207a03f97a
alvcode <alvcode@ya.ru>
2023-10-01 18:14:41 +0300

    first commit

---
+++ src/layouts/default/AuthDefault.vue
@@ -0,0 +1,10 @@
+<template>
+  <v-app>
+    <v-img height="100" src="@/assets/logo.svg" />
+    <default-view />
+  </v-app>
+</template>
+
+<script setup>
+  import DefaultView from './View.vue'
+</script>
---
+++ src/views/Login.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="login">
+    <div class="d-flex align-center justify-center" style="height: 80vh">
+        <v-sheet width="400" class="mx-auto">
+            <v-form fast-fail @submit.prevent="login">
+                <v-text-field variant="outlined" v-model="email" label="E-mail"></v-text-field>
+
+                <v-text-field variant="outlined" v-model="password" label="Password"></v-text-field>
+
+                <v-btn type="submit" color="primary" block class="mt-2">Войти</v-btn>
+
+            </v-form>
+        </v-sheet>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "Login",
+  components: {
+    
+  },
+  data() {
+    return {
+      email: '',
+      password: ''  
+    };
+  },
+  computed: {
+    
+  },
+  methods: {
+    login() {
+        // Your login logic here
+    },
+  },
+  created() {
+    
+  }
+};
+</script>
+
+<style scoped lang="scss">
+
+</style>
